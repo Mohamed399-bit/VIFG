e85f34601c1d170c182915ebe4fce676f470628e
Mohamed Abdeen <mohamedabden399@gmail.com>
2023-05-17 16:13:59 +0300

    test case for back to exist initiative

---
+++ src/test/java/InitiativesCompleteDataFeatures/BackToExistInitiativePageTest.java
@@ -0,0 +1,59 @@
+package InitiativesCompleteDataFeatures;
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import pages.*;
+import tests.TestBase;
+import utilities.Helper;
+
+public class BackToExistInitiativePageTest extends TestBase {
+
+    InitiativeDetailsPage initiativeDetailsObject;
+    initiativeFundingStatusFiledPage initiativeFundingStatusFiledObject;
+    InitiativeClassificationPage initiativeClassificationObject;
+    InitiativeAchievedSavingsPage initiativeAchievedSavingsObject;
+    OwnerEntityPage ownerEntityObject;
+    CalenderPage calenderObject;
+    @Test(priority = 4)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Back To Exist Initiative Page")
+    @Feature("Back To Exist Initiative Page")
+    @Epic("Uncompleted Initiative")
+    public void BackToExistInitiativePage() throws InterruptedException {
+
+        initiativeDetailsObject = new InitiativeDetailsPage(driver);
+        initiativeDetailsObject.enterStrategicObjective(Helper.generateRandomName(300));
+        initiativeDetailsObject.clickOnOwnerList();
+
+        ownerEntityObject = new OwnerEntityPage(driver);
+        ownerEntityObject.SelectEntity();
+
+        initiativeDetailsObject.clickOnInitiativeFundingStatus();
+
+        initiativeFundingStatusFiledObject = new initiativeFundingStatusFiledPage(driver);
+        initiativeFundingStatusFiledObject.SelectInitiativeFundingStatus();
+
+        initiativeDetailsObject.clickOnInitiativeClassification();
+
+        initiativeClassificationObject = new InitiativeClassificationPage(driver);
+        initiativeClassificationObject.SelectInitiativeClassification();
+
+        initiativeDetailsObject.enterApprovedCosts(Helper.generateRandomNumber(5)+"000");
+        initiativeDetailsObject.clickOnEditIcon();
+
+        initiativeAchievedSavingsObject = new InitiativeAchievedSavingsPage(driver);
+        initiativeAchievedSavingsObject.enterInitiativeAchievedSavings(Helper.generateRandomNumber(3)+"000");
+
+        initiativeDetailsObject.enterInitiativeDescription(Helper.generateRandomName(10000));
+        initiativeDetailsObject.clickOnCalenderIcon();
+
+        calenderObject = new CalenderPage(driver);
+        calenderObject.selectMonthByIndex(5);
+        calenderObject.selectYearByIndex(4);
+        calenderObject.selectDay("15");
+
+        initiativeDetailsObject.enterInitiativeDurationPerMonth(Helper.generateRandomNumber(1));
+        initiativeDetailsObject.clickOnBackButton();
+
+    }
+}
