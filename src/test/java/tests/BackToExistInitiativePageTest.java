d79dc30698f3ed6d345df3a5308647c7cf1e3a41
Mohamed Abdeen <mohamedabden399@gmail.com>
2023-05-17 16:16:36 +0300

    test cases for all acrion in complete initiative data

--- src/test/java/tests/BackToExistInitiativePageTest.java
+++ src/test/java/tests/BackToExistInitiativePageTest.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) NCGR 2023.
- * All Rights Reserved.
- *
- * ver          Developer          Date        Comments
- * ----- ---------------------  ----------  ----------------------------------------
- * 1.00  Eng. Mohamed Abden 08/05/2023 - Script created.
- */
-
 package tests;
 
 import io.qameta.allure.*;
@@ -14,43 +5,20 @@ import org.testng.annotations.Test;
 import pages.*;
 import utilities.Helper;
 
-public class MohamedTest extends TestBase{
+public class BackToExistInitiativePageTest extends TestBase{
 
-    LunchPage lunchObject;
-    LoginPage loginObject;
-    HomePage homeObject;
-    ExistInitiativesPage  existInitiativesObject;
     InitiativeDetailsPage initiativeDetailsObject;
     initiativeFundingStatusFiledPage initiativeFundingStatusFiledObject;
     InitiativeClassificationPage initiativeClassificationObject;
     InitiativeAchievedSavingsPage initiativeAchievedSavingsObject;
     OwnerEntityPage ownerEntityObject;
     CalenderPage calenderObject;
-
-    @Test(priority = 1)
+    @Test(priority = 4)
     @Severity(SeverityLevel.CRITICAL)
-    @Description("Login To Web")
-    @Feature("Login Epic")
-    @Epic("Login Epic")
-    public void testMohamed() throws InterruptedException {
-
-        lunchObject = new LunchPage(driver);
-        lunchObject.clickOnGovernmentEntity();
-
-        loginObject = new LoginPage(driver);
-        loginObject.loginMethod("1856103492","Test@12345");
-
-        homeObject = new HomePage(driver);
-        homeObject.verifyThatUserLoggedIn("المبادرات القائمة");
-       //homeObject.clickOnExistInitiatives();
-
-        existInitiativesObject = new ExistInitiativesPage(driver);
-        existInitiativesObject.clickOnUnCompletedInitiatives();
-        Thread.sleep(2000);
-        existInitiativesObject.getDataForFirstInitiative();
-        //existInitiativesObject.clickOnCompletedInitiatives();
-        existInitiativesObject.clickOnEditInitiativeNumber(existInitiativesObject.UICNumber);
-        System.out.println(existInitiativesObject.initiativeNameTxt);
+    @Description("Back To Exist Initiative Page")
+    @Feature("Back To Exist Initiative Page")
+    @Epic("Uncompleted Initiative")
+    public void BackToExistInitiativePage() throws InterruptedException {
 
         initiativeDetailsObject = new InitiativeDetailsPage(driver);
         initiativeDetailsObject.enterStrategicObjective(Helper.generateRandomName(300));
@@ -84,10 +52,7 @@ public class MohamedTest extends TestBase{
         calenderObject.selectDay("15");
 
         initiativeDetailsObject.enterInitiativeDurationPerMonth(Helper.generateRandomNumber(1));
-        //initiativeDetailsObject.clickOnCancelButton();
         initiativeDetailsObject.clickOnBackButton();
-
-        Thread.sleep(10000);
 
     }
 }
